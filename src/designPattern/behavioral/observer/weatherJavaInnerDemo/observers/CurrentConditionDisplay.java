85336372b9bf76d7dfd31102f7bab29b8bbdd0cb
zhiwei.liu <lzw6810@qq.com>
2019-09-08 12:16:25 +0800

    观察者模式

---
+++ src/designPattern/behavioral/observer/weatherJavaInnerDemo/observers/CurrentConditionDisplay.java
@@ -0,0 +1,30 @@
+package designPattern.behavioral.observer.weatherJavaInnerDemo.observers;
+
+import designPattern.behavioral.observer.weatherJavaInnerDemo.subjects.WeatherData;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * @author zhiwei.liu003
+ * @date 2019/9/810:59
+ */
+public class CurrentConditionDisplay  implements Observer {
+    private  double temperature;
+    private  double humidity;
+    private  double pressure;
+
+    @Override
+    public void update(Observable o, Object data) {
+        this.temperature =((WeatherData.Data)data).getTemperature();
+        this.humidity =((WeatherData.Data)data).getHumidity();
+        this.pressure =((WeatherData.Data)data).getPressure();
+        display();
+    }
+
+    private void display() {
+        System.out.println("CurrentDisplay:"+temperature+
+                ":"+humidity+
+                ":"+pressure);
+    }
+}
