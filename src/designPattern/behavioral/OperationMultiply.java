3f6c07f8f7d4c9dba8bee287bd016790cabe9c1e
zhiwei.liu <lzw6810@qq.com>
2019-09-08 09:17:40 +0800

    策略模式

---
+++ src/designPattern/behavioral/OperationMultiply.java
@@ -0,0 +1,12 @@
+package designPattern.behavioral;
+
+/**
+ * @author zhiwei.liu003
+ * @date 2019/9/89:04
+ */
+public class OperationMultiply implements Strategy {
+    @Override
+    public int doOperation(int num1, int num2) {
+        return num1 * num2;
+    }
+}
---
+++ src/designPattern/behavioral/Strategy.java
@@ -0,0 +1,13 @@
+package designPattern.behavioral;
+
+/**
+ * @author zhiwei.liu003
+ * @date 2019/9/89:05
+ */
+
+/**
+ * 策略公共接口
+ */
+public interface Strategy {
+    int doOperation(int num1, int num2);
+}
