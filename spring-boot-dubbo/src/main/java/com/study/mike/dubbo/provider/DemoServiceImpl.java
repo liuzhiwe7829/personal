08146a8e5baae22b7579be16af3ea77bbd59a37f
zhiwei.liu <lzw6810@qq.com>
2020-02-27 08:25:35 +0800

    rpc

---
+++ spring-boot-dubbo/src/main/java/com/study/mike/dubbo/provider/DemoServiceImpl.java
@@ -0,0 +1,13 @@
+package com.study.mike.dubbo.provider;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.study.mike.dubbo.DemoService;
+
+@Service
+public class DemoServiceImpl implements DemoService {
+	@Override
+    public String sayHello(String name) {
+		System.out.println("*********************** " + name);
+		return "Hello " + name;
+	}
+}
